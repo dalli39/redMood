a6e24a6e6e7a7db717a9c717b05d6f2c0932e583
dalli39 <72554367+dalli39@users.noreply.github.com>
2022-07-05 15:04:54 +0430

    Add files via upload

---
+++ sample_config.py
@@ -0,0 +1,9 @@
+import os
+
+
+class Config(object):
+    TG_BOT_TOKEN = "5457445330:AAHBqouRXAXNrMah50yPnmbNvRgNb89czXA"
+
+    APP_ID = 17238427
+
+    API_HASH ="9a9ec6daad516a7d7f8017f7822b5cdb"
